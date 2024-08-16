049f01292eb8f5cb3d0edff4bde6da8d02c322d3
devaryakjha <dev.jha.arya@gmail.com>
2024-08-16 07:02:33 +0530

    refactor: Update OnLinkTap typedef to use Object instead of String for attributes

--- lib/src/typedefs.dart
+++ lib/src/typedefs.dart
@@ -3,6 +3,6 @@ import 'package:html_to_flutter/html_to_flutter.dart';
 /// Signature for tap event handler.
 typedef OnLinkTap = void Function(
   String? url, [
-  Map<String, String>? attributes,
+  Map<Object, String>? attributes,
   HTMLElement? element,
 ]);
